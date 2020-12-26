49dbad7ec0605001ed45e7b018392b6793827f2a
albin lamichhane <spell.albin@gmail.com>
2020-12-26 13:54:01 +0545

    changes

--- twitter-bot/bot/urls.py
+++ twitter-bot/bot/urls.py
@@ -5,9 +5,9 @@ from bot import views
 
 urlpatterns = [
     #path('', views.index, name='index'),
-    path('bota/', views.bota, name='bota'),
+    path('markovbot/', views.bota, name='bota'),
     # path('botc/', views.botc, name='botc'),
-    path('botb/', views.botb, name='botb'),
+    path('xiaomibot/', views.botb, name='botb'),
     path('', views.home, name='homepage'),
 
 
